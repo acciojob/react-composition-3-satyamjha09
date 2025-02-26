6b0b327a549b0c1255d58cac1bc724565ac63fc5
satyamjha <satyamjhaactor07@gmail.com>
2025-02-26 18:02:10 +0530

    finally Done Change

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import Tooltip from "./Tooltip";
 
 const App = () => {
-  
   return (
-    <div style={{ padding: "50px", textAlign: "center" }}>
-      
-
-      <Tooltip text="This is a Tooltip">
-        <p className="tooltip">Hover over this paragraph</p>
+    <div style={{ padding: "20px" }}>
+      <h1>React Tooltip Component</h1>
+      <Tooltip text="This is a tooltip!">
+        <button>Hover over me</button>
       </Tooltip>
-
-      <Tooltip text="This is another Tooltip">
-        <button className="tooltip">Hover over this button</button>
+      <br /><br />
+      <Tooltip text="Another tooltip example">
+        <span style={{ textDecoration: "underline", cursor: "pointer" }}>
+          Hover over this text
+        </span>
       </Tooltip>
     </div>
   );
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,18 +1,18 @@
+// Tooltip.js
 import React, { useState } from "react";
 import "./Tooltip.css";
 
 const Tooltip = ({ text, children }) => {
-
   const [visible, setVisible] = useState(false);
 
   return (
-    <div 
-      className="tooltip-container"
+    <div
+      className="tooltip"
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
     >
-      <div className="tooltip-target"> {children} </div>
-      {visible && <div className="tooltip-text"> {text} </div>}
+      {children}
+      {visible && <div className="tooltiptext">{text}</div>}
     </div>
   );
 };
